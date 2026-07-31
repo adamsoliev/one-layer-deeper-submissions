556008bf15073979b9b1bcdbcdeaf8abe1577055
adamsoliev <asoliyev13@gmail.com>
2026-07-30 23:01:36 -0500

    registered dual quotient orbit

--- schema.sql
+++ schema.sql
@@ -42,7 +42,8 @@ INSERT INTO architectures VALUES
     ('straight-through', 'Straight-through semantic residue recurrence', 'b08fdf173989a55f54c1019578815d1b5e24d929', 34),
     ('supervised-table', 'Supervised transition-table recurrence', 'c9fafeb21d846670664da42d455dc949d79f0664', 35),
     ('binary-semantic', 'Binary-compositional supervised recurrence', '689647d9790c453cd14e082e7ec24c00f0ecc700', 36),
-    ('modulus-program', 'Modulus-programmed supervised recurrence', 'e7a1794e432e3ecabfac6c5a3b39ba972aa321ce', 37)
+    ('modulus-program', 'Modulus-programmed supervised recurrence', 'e7a1794e432e3ecabfac6c5a3b39ba972aa321ce', 37),
+    ('dual-orbit', 'Dual-quotient orbit recurrence', '45f01b75dc3873497b5805a3af0522e8c883fcff', 38)
 ON CONFLICT (architecture_key) DO UPDATE SET
     architecture_label = excluded.architecture_label,
     source_commit = excluded.source_commit,
